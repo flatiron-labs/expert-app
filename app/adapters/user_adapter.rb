c3ae62f7678212677ff95a2157c2ab8a20f689d4
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-09-22 14:17:03 -0400

    Build importer for users from CSV

--- app/adapters/user_adapter.rb
+++ app/adapters/user_adapter.rb
@@ -1,4 +1,13 @@
+require 'csv'
+
 class UserAdapter
 
+  def self.import
+    csv_text = File.read('db/csv_data/learn-experts.csv')
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      User.find_or_create_by(row.to_hash)
+    end
+  end
 
 end
