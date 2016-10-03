e98dc14a081bb7e8e6f6725269a4b86b04c1fc84
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-03 14:09:58 -0400

    Add method to HRQ to get Q count by hour for a given date range

--- app/models/handraise_question.rb
+++ app/models/handraise_question.rb
@@ -1,2 +1,20 @@
 class HandraiseQuestion < ActiveRecord::Base
+
+  def self.date_range_count(start_date, end_date)
+    sql = <<-SQL
+      SELECT DATE_TRUNC('hour', asked_at) as hourly,
+        COUNT(*) AS q_count
+      FROM handraise_questions
+      WHERE asked_at >= $1
+        AND asked_at <= $2
+      GROUP BY hourly
+    SQL
+    # query = ActiveRecord::Base.connection.execute(sql, start_date.to_s, end_date.to_s)
+
+    connection = ActiveRecord::Base.connection.raw_connection
+    connection.prepare('some_other_name', sql)
+    results = connection.exec_prepared('some_other_name', start_date, end_date)
+    results.to_a
+  end
+
 end
