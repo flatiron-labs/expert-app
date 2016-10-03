1e462ffc09bcb9cd0f940461731fa9b596d0c710
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-03 17:51:43 -0400

    Refine hrq date_range_count method

--- app/models/handraise_question.rb
+++ app/models/handraise_question.rb
@@ -2,8 +2,9 @@ class HandraiseQuestion < ActiveRecord::Base
 
   def self.date_range_count(start_date, end_date)
     sql = <<-SQL
-      SELECT DATE_TRUNC('hour', asked_at) as hourly,
-        COUNT(*) AS q_count
+      SELECT
+        DATE_TRUNC('hour', asked_at) as hourly,
+        COUNT(handraise_questions.id) AS q_count
       FROM handraise_questions
       WHERE asked_at >= $1
         AND asked_at <= $2
@@ -11,8 +12,7 @@ class HandraiseQuestion < ActiveRecord::Base
     SQL
 
     connection = ActiveRecord::Base.connection.raw_connection
-    connection.prepare('some_other_name', sql)
-    results = connection.exec_prepared('some_other_name', start_date, end_date)
+    results = connection.exec_params(sql, [start_date, end_date])
     
     results.to_a
   end
