2b1213b3040b50176b5b6b15d46e09d1fb992af8
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-03 17:52:07 -0400

    Add methods to shift to get expert load

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,4 +5,32 @@ class Shift < ActiveRecord::Base
 
   validates :start_time, :end_time, presence: true
 
+  def self.expert_load(start_date, end_date)
+    sql = <<-SQL
+      SELECT  start_time, end_time,
+        COUNT(user_id) AS experts_on
+      FROM shifts
+      RIGHT JOIN schedules
+        ON shifts.id = schedules.shift_id
+      WHERE start_time >= $1
+        AND start_time <= $2
+      GROUP BY start_time, end_time
+    SQL
+
+    connection = ActiveRecord::Base.connection.raw_connection
+    results = connection.exec_params(sql, [start_date, end_date])
+
+    results.to_a
+  end
+
+  def self.shift_by_hour(hour)
+    binding.pry
+    where("start_time <= #{hour} AND end_time >= #{(Time.parse(hour) + 1.hour).to_s(:db)}")
+  end
+
+  def self.load_by_hour(hour)
+    shift = shift_by_hour(hour)
+    shift ? shift.count : 0
+  end
+
 end
