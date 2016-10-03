294020626a7c3e0dc0574bee31a89917668ebaa5
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-03 14:09:10 -0400

    Remove commented out lines from the handraise questions adapter

--- app/adapters/handraise_questions_adapter.rb
+++ app/adapters/handraise_questions_adapter.rb
@@ -9,16 +9,7 @@ class HandraiseQuestionsAdapter
       csv = CSV.parse(csv_text, :headers => true)
       csv.each do |row|
         row_hash = row.to_hash
-
         HandraiseQuestion.find_or_create_by(row_hash)
-
-        # unless HandraiseQuestion.find_by(id: row_hash[:id])
-        #   binding.pry
-        #
-        # end
-        # shift = find_or_create_shift_by_row(row_hash)
-        # expert = User.find_or_create_by_full_name(row_hash["Employee"])
-        # Schedule.find_or_create_by(user_id: expert.id, shift_id: shift.id)
       end
     end
   end
