a0171392211b33b0da4d32b60788669f8d0af115
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-07 11:26:37 -0400

    Add javascript to fetch and data for a date_range

--- app/assets/javascripts/shift_loads.js
+++ app/assets/javascripts/shift_loads.js
@@ -1,12 +1,32 @@
 
-var runOnReady = () =>{
+let runOnReady = () =>{
   console.log('this worked')
   attachListenerToDatePicker()
 }
 
-var attachListenerToDatePicker = () => {
+let handelFormSubmission = () => {
+  let start_date = document.getElementById('start_date').value
+  let end_date = document.getElementById('end_date').value
+  let body = {start_date: start_date, end_date: end_date}
+  fetch(`/shift_loads_data?start_date=${start_date}&end_date=${end_date}`)
+    .then(res => {
+      debugger;
+      return res.json()
+    })
+    .then((json) =>{
+      debugger;
+    })
+    .catch(error => {
+      debugger;
+    })
+}
+
+let attachListenerToDatePicker = () => {
   let submit = document.getElementById('submit')
-  debugger;
+  $(submit).on('click', (e)=>{
+    e.preventDefault();
+    handelFormSubmission();
+  })
 }
 
 $(document).ready(runOnReady);
