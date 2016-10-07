5c8b2729881353820c2e3c1f4e314288c1cc1cda
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-07 11:30:50 -0400

    Add route to get expert load data

--- app/controllers/shift_loads_controller.rb
+++ app/controllers/shift_loads_controller.rb
@@ -5,7 +5,10 @@ class ShiftLoadsController < ApplicationController
   end
 
   def data
-
+    qss = QuestionShiftService.new(params['start_date'], params['end_date'])
+    # binding.pry
+    @shift_loads = qss.shift_load
+    render :json => @shift_loads
   end
 
 end
