f766ada6f933e86e026429fb7a51939f4b99eac5
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-07 11:31:51 -0400

    Update question shift service to return data in format for ChartJS

--- app/services/question_shift_service.rb
+++ app/services/question_shift_service.rb
@@ -13,15 +13,26 @@ class QuestionShiftService
       .beginning_of_day.utc.to_s
   end
 
+  def blank_export_hash
+    {labels:[], experts_on:[], q_count:[]}
+  end
+
   def shift_load
     hrq_load = HandraiseQuestion.date_range_count(start_date, end_date)
     expert_load = Shift.expert_load(start_date, end_date)
-    hrq_load.each_with_object({}) do |question_hour, export_hash|
+    hrq_load.each_with_object(blank_export_hash) do |question_hour, export_hash|
       time = question_hour["hourly"]
       expert_load = Shift.load_by_hour(time)
       time_et = DateTime.parse(time).in_time_zone('Eastern Time (US & Canada)').strftime('%Y-%m-%d-%H:00')
-      export_hash[time_et] = {experts_on: expert_load, q_count: question_hour["q_count"]}
+      # export_hash[time_et] = {experts_on: expert_load, q_count: question_hour["q_count"]}
+      add_to_hash(export_hash, time_et, expert_load, question_hour["q_count"])
     end
+  end
+
+  def add_to_hash(export_hash, time_in_et, expert_load, q_count)
+    export_hash[:labels] << time_in_et
+    export_hash[:experts_on] << expert_load
+    export_hash[:q_count] << q_count
   end
 
 end
