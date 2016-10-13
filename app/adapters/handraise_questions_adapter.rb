4eb167f7f849a7a2c61084fec84e06d1c56babf4
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-13 15:17:55 -0400

    allow adapter to skip importing old files

--- app/adapters/handraise_questions_adapter.rb
+++ app/adapters/handraise_questions_adapter.rb
@@ -7,9 +7,20 @@ class HandraiseQuestionsAdapter
     files_to_import.each do | path_to_file |
       csv_text = File.read(path_to_file)
       csv = CSV.parse(csv_text, :headers => true)
-      csv.each do |row|
-        row_hash = row.to_hash
-        HandraiseQuestion.find_or_create_by(row_hash)
+
+      last_question_id = csv[-1].to_hash["learn_id"]
+
+      unless HandraiseQuestion.find_by(learn_id: last_question_id)
+        import_csv(csv)
+      end
+    end
+  end
+
+  def self.import_csv(csv)
+    csv.each do |row|
+      row_hash = row.to_hash
+      unless HandraiseQuestion.find_by(learn_id: row_hash["learn_id"])
+        HandraiseQuestion.create(row_hash)
       end
     end
   end
