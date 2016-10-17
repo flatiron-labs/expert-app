1370ef789a9429f466025b68e723e7f567eb6b10
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-17 11:14:53 -0400

    remove pry

--- app/controllers/shift_loads_controller.rb
+++ app/controllers/shift_loads_controller.rb
@@ -10,7 +10,6 @@ class ShiftLoadsController < ApplicationController
 
   def data
     qss = QuestionShiftService.new(params['start_date'], params['end_date'])
-    # binding.pry
     @shift_loads = qss.shift_load
     render :json => @shift_loads
   end
